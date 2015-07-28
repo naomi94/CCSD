76e202e556124b5c60eb86a8908b53c7d4714e3d
naomi94 <naomisontrop@yahoo.com>
2015-07-28 22:31:36 +0300

    Test that we can read from newly created deployment item

---
+++ CCSD.Tests/ParsingTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace CCSD.Tests
+{
+    [TestClass]
+    public class ParsingTests
+    {
+        [TestMethod]
+        [DeploymentItem("data/ccsd.csv")]
+        public void CanReadTestFile()
+        {
+            var ccsd = File.ReadAllLines("ccsd.csv");
+        }
+    }
+}
