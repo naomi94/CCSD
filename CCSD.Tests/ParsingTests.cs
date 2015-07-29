d3874ef441c5d1fb6f3b80cf64f4da4f95a4c8ec
naomi94 <naomisontrop@yahoo.com>
2015-07-29 22:53:03 +0300

    Added unit test and added back the parser class

--- CCSD.Tests/ParsingTests.cs
+++ CCSD.Tests/ParsingTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
+using System.Collections.Generic;
 
 namespace CCSD.Tests
 {
@@ -13,7 +14,16 @@ namespace CCSD.Tests
         {
             var parser = new ClinicalCodesParser();
             var codes = parser.Parse("ccsd.csv");
-            Assert.AreEqual(2732, codes.Count);
+            Assert.AreEqual(2731, codes.Count);
+        }
+
+        [TestMethod]
+        [DeploymentItem("data/onlytableheader.csv")]
+        public void ParserIgnoresTableHeader()
+        {
+            var parser = new ClinicalCodesParser();
+            var codes = parser.Parse("onlytableheader.csv");
+            Assert.AreEqual(0, codes.Count);
         }
     }
 }
---
+++ CCSD/ClinicalCodesParser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CCSD
+{
+    public class ClinicalCodesParser
+    {
+        public List<ClinicalCode> Parse(string filename)
+        {
+            var records = File.ReadAllLines(filename).Skip(1);
+
+            var codes = new List<ClinicalCode>();
+
+            foreach (var record in records)
+            {
+                var tokens = record.Split('\t');
+
+                var code = new ClinicalCode();
+                
+                codes.Add(code);
+            }
+
+            return codes;
+        }
+    }
+}
